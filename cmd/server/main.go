ec01c2fab8452395b51c0d7dfa91cd33ac068950
amandx36 <xamandeep360@gmail.com>
2026-07-05 10:20:43 +0530

    feature: implemented all api end points

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"LogSentry/internal/api/routes"
 	"LogSentry/internal/config"
@@ -11,28 +12,37 @@ import (
 )
 
 func main() {
+	gin.SetMode(gin.ReleaseMode)
+
+	start := time.Now()
+
+
+
+	step := time.Now()
 	cfg, err := config.Loadconfig("internal/config/config.json")
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Printf("[Startup] Config loaded in %v", time.Since(step))
 
+	step = time.Now()
 	db, err := postgres.Connect(cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
+	log.Printf("[Startup] Database connected in %v", time.Since(step))
 
+	step = time.Now()
 	api := gin.Default()
+	log.Printf("[Startup] Gin engine created in %v", time.Since(step))
 
-	routes.HealthCheckEP(api)
-	routes.ApiVersionEP(api)
-	routes.DashBoardEP(api, db)
-	routes.SearchByCategoryEP(api,db)
-	routes.SearchBySourceEP(api,db)
-	routes.SearchByKeyWordsEP(api,db)
-	routes.GetRecentLogEP(api,db)
-	routes.SearchByDateEP(api, db)
-	routes.AnalyticsEP(api, db)
+	step = time.Now()
+	routes.RegestAllRoutes(api, db)
+	log.Printf("[Startup] Routes registered in %v", time.Since(step))
+
+	log.Printf("[Startup] Total startup time: %v", time.Since(start))
+	log.Println("[Startup] Server listening on http://localhost:8080")
 
 	if err := api.Run(":8080"); err != nil {
 		log.Fatal(err)
