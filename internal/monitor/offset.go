e919268fb02a817197196547c0ce27d907fd2acc
amandx36 <xamandeep360@gmail.com>
2026-07-06 19:54:45 +0530

    feature: implemented the thread safty of offset dude

--- internal/monitor/offset.go
+++ internal/monitor/offset.go
@@ -1,18 +1,31 @@
 package monitor 
 
+import (
+	"sync"
+)
 type OffsetManager struct {
+	mu sync.RWMutex
 	offset  map[string]int64
 }
 // making a constructor for  offset to prevent nil map error 
 func NewOffsetManager() *OffsetManager  {
+	// adding mutex 
+	
 	return &OffsetManager{
 		offset: make(map[string]int64),
 	}
 }
 
+// read lock 
 func (of *OffsetManager) GetOffset(filename string) int64 {
-	return of.offset[filename]
+    of.mu.RLock()     
+    defer of.mu.RUnlock()
+    return of.offset[filename]
 }
+// write lock 
 func (of *OffsetManager) UpdateOffset(fileName string, offset int64) {
+	// write lock 
+	of.mu.Lock()
+	defer of.mu.Unlock()	
 	of.offset[fileName] = offset
 }
